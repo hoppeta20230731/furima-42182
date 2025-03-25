b954d86c450e8de84e2a2db24a6e205e63137b28
hoppeta20230731 <sra6road6mt@gmail.com>
2025-03-25 15:50:06 +0900

    バリデーション修正
    ・with_options使用

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -2,14 +2,16 @@ class OrderShippingAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含めてください' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'は選択してください' }
-  validates :city, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の数字で入力してください' }
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含めてください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'は選択してください' }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の数字で入力してください' }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,13 @@ class User < ApplicationRecord
   NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :first_name, presence: true, format: { with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :family_name_rubi, presence: true, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
-  validates :first_name_rubi, presence: true, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
-  validates :date_of_birth, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :first_name, format: { with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :family_name_rubi, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+    validates :first_name_rubi, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+    validates :date_of_birth
+  end
   validates :password, format: { with: PASSWORD_REGEX, message: 'は半角英字と数字の両方を含めて設定してください' }
 end
