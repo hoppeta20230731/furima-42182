4649d7173f681516307313b836c4026e5d2ed4e6
hoppeta20230731 <sra6road6mt@gmail.com>
2025-04-22 16:53:51 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# ★★★ この行を追加 ★★★
+set :bundle_flags, '--deployment --without development:test --jobs 4'
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
